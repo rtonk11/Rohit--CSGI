f0a8fd864925f7ab7f17564a3ea7bbb3fc259e64
Rohit Tonk <ROHITTO@amdocs.com>
2025-04-14 01:08:54 +1000

    more files

---
+++ src/main/java/com/example/rohitcsgi/csgi_sample/CsgiSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.rohitcsgi.csgi_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsgiSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsgiSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rohitcsgi/csgi_sample/CsgiSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rohitcsgi.csgi_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsgiSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
